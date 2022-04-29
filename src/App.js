7a6327eb4bf05896eab311436469bb71a2d5a7e2
alestojanovic <alestojanovic@ebay.com>
2022-04-29 07:30:20 +0200

    fix: refactoring files, added messages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Three from "./components/Three";
-import { One } from "./components/One";
+import One from "./components/One";
 import { Two } from "./components/Two";
+import { Three } from "./components/Three";
 import { PrivateRoutes } from "./PrivateRoutes";
 import "./App.css";
 
 const App = () => {
-  const time = new Date();
-  time.setSeconds(time.getSeconds() + 86400);
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path={PrivateRoutes.PARAM_THREE} element={<Three />} />
           <Route path={PrivateRoutes.PARAM_ONE} element={<One />} />
           <Route path={PrivateRoutes.PARAM_TWO} element={<Two />} />
+          <Route path={PrivateRoutes.PARAM_THREE} element={<Three />} />
         </Routes>
       </Router>
     </div>
--- src/components/Two.jsx
+++ src/components/Two.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { PrivateRoutes } from "../PrivateRoutes";
-import logo from "../logo.svg";
+import { MessageTwo } from "../Messages";
 
 export const Two = () => {
   const navigate = useNavigate();
@@ -9,23 +9,12 @@ export const Two = () => {
     navigate(`${PrivateRoutes.PARAM_THREE}`);
   };
   return (
-    <>
-      <div>
-        <button className="two-button" onClick={routeChange}>
-          <img src={logo} className="App-logo" alt="logo" /> <br />
-          Download me and find the gift inside.
-        </button>
-      </div>
-      <div className="footer">
-        <a
-          className="footer-link"
-          href="https://github.com/alestojanovic/scavenger-hunt"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          https://github.com/alestojanovic/scavenger-hunt
-        </a>
-      </div>
-    </>
+    <div>
+      <h3>{MessageTwo.HINT}</h3>
+      <button className="one-button" onClick={routeChange}>
+        {" "}
+        {MessageTwo.PEP}{" "}
+      </button>
+    </div>
   );
 };
